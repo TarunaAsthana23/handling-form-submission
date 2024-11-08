23ef2bb6d4c861cb1f0e46013fc4856f7e9cfe65
Taruna Asthana <tasthana@swnindia.org>
2024-11-08 11:33:30 +0530

    Initial commit

---
+++ src/main/java/com/java/handling_form_submission/HandlingFormSubmissionApplication.java
@@ -0,0 +1,14 @@
+package com.java.handling_form_submission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlingFormSubmissionApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HandlingFormSubmissionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/java/handling_form_submission/HandlingFormSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.handling_form_submission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlingFormSubmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
